96077eb83a2d21e0bcf63d67fbcfd9cdaf38a636
Varun Dixit <77varundixit@gmail.com>
2021-10-24 19:08:01 +0530

    solution

--- functions/question2.py
+++ functions/question2.py
@@ -7,11 +7,29 @@ hi= 125
 Make this a one or more function program
 '''
 
-def file_c(filename):
-    file = open('filename')
-    data = filename.read()
-    words = data.split()
-    print('Number of words in text file :', len(words))
+from string import punctuation
 
+def question1_solver(read_file_path,save_file_path='result.txt'):
+    # read the file and store in a variable
+    file = open(read_file_path, encoding='utf-8')
+    content = file.read()
+    file.close()
 
-file_c('frank.txt')
+    # remove every punctuations
+    for punc in punctuation:
+        content= content.replace(punc,'')
+
+    # make every thing lowercase and split file in words
+    words = content.casefold().split()
+    print(f'total words => {len(words)}')
+
+    # create a empty file
+    with open(save_file_path,'w') as file2: # create
+        # counting the words
+        for word in set(words):
+            count = words.count(word)           # creating words
+            file2.write(f'{word}\t{count}\n')   # storing in file
+
+# the end
+
+question1_solver('frank.txt')
